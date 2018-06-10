40412e9ae05faf8d7039a9c68f5995fcb0613669
PaxPax <ehaysle@siue.edu>
2018-06-10 02:44:43 -0500

    created a working parent script file

--- scripts/parent_script.py
+++ scripts/parent_script.py
@@ -1,18 +1,35 @@
-import threading
+from threading import Thread
 import configparser
 import discord_webhook, user_email, slack_webhook, text, twitter
 
 def get_selected_options():
-    tokens = {}
+    temp = {}
     config = configparser.ConfigParser()
-    with open ('../config_folder/selected_options.cfg') as fp:
-        config.readfp(fp)
-    
+    try:
+        with open ('../config_folder/selected_options.cfg') as fp:
+            config.readfp(fp)
+    except IOError:
+        print('An error has occurred reading the file.')
+
     for key in config['selected']:
-        tokens[key] = config['selected'][key]
+        temp[key] = config['selected'][key]
 
-    return tokens
-    
-        
+    return temp
 
-get_selected_options()
+def check_selected_options():
+    selected_values = get_selected_options()
+    imported_values = [discord_webhook, user_email, slack_webhook, text, twitter]
+    tmp_value = []
+    for key in selected_values:
+        if selected_values[key] == "False":
+            for values in imported_values:
+                if key in str(values):
+                    tmp_value.append(values)
+    return tmp_value
+
+def begin_threads():
+    modules_to_start = check_selected_options()
+    for tt in modules_to_start:
+        Thread(target=tt).start()
+
+begin_threads()
