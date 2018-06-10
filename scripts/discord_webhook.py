e30e893039abe66f9ce8f32a2c46319ea2eb1367
PaxPax <ehaysle@siue.edu>
2018-06-10 03:15:09 -0500

    flushed out the discord_webhook.py file

--- scripts/discord_webhook.py
+++ scripts/discord_webhook.py
@@ -1,4 +1,53 @@
 import requests
+import configparser
 
-payload = {'username': 'timmy', 'content': 'hey ugly'}
-send_message = requests.post('https://discordapp.com/api/webhooks/366068895024545795/qwLLhENE5sWUvGK2W9I0K1i83P0qKtugXGyZNyiGap3zpgRJD5-nWGGXkIKbGSve9-8P',data=payload)
+#first import settings
+def read_discord_config():
+    config = configparser.ConfigParser()
+    temp = {}
+    try:
+        with open('../config_folder/discord_config.cfg') as fp:
+            config.readfp(fp)
+    except IOError:
+        print('An error has occured reading the file')
+    
+    for key in config['discord']:
+        temp[key] = config['discord'][key]
+
+    return temp
+
+#next import the message
+def read_message():
+    try:
+        with open('../message/user_message.txt', 'r') as out_file:
+            return out_file.read()
+    except IOError:
+        print('An error has occurred reading the files')
+
+def get_webhook():
+    temp = {}
+    config = configparser.ConfigParser()
+    try:
+        with open('../config_folder/discord_config.cfg') as fp:
+            config.readfp(fp)
+    except IOError:
+        print('An error has occured reading the file')
+    
+    for key in config['webhook']:
+        temp[key] = config['discord'][key]
+
+    return temp
+
+#create the payload
+def create_payload():
+    payload = {}
+    config_values = read_discord_config()
+    webhookurl = get_webhook()
+
+    for key in config_values:
+        payload[key] = config_values[key]
+    payload['content'] = read_message()
+    
+    send_message = requests.post(webhookurl, data=payload)
+
+create_payload()
